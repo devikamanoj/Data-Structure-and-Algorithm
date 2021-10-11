d88a3ea48ba7ab6743896da208456b6a3b85601c
Devika Manoj <dmenon138@gmail.com>
2021-10-12 01:38:38 +0530

    errors to be solved

--- LongestPrefix/src/U4AIE20144_A3.java
+++ LongestPrefix/src/U4AIE20144_A3.java
@@ -4,7 +4,6 @@ public class U4AIE20144_A3
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter testcases: ");
         int TC = in.nextInt();
         in.nextLine();
         for(int i=0;i<TC ; i++)
@@ -26,6 +25,7 @@ class KMP_Search
     int P;
     String [] txt;
     String [] pattern;
+    int loc=0;
     void PatternSearch(String pat, String text)
     {
         T = text.length();
@@ -45,27 +45,112 @@ class KMP_Search
             pattern[i]=pat.charAt(i) + " ";
         }
 
-        //pattern checking
-        patternCheck(txt, pattern);
-    }
-    void patternCheck(String [] txt, String [] pattern)
-    {
-        int i=0;
-        int j=0;
-        while(i<T)
+        String mat = printLCSubStr(txt, pattern,T,P);
+        mat=mat.replace(" ", "");
+        int index = text.indexOf(mat);
+        System.out.print(prefix(text, pat)+" ");
+        System.out.print(LCSubStr(txt, pattern,T,P)+" "); //3
+        if (LCSubStr(txt, pattern,T,P)==0)
         {
-            while(j<P)
+            System.out.println("-1");
+        }
+        else
+            System.out.println(index);
+
+    }
+    int LCSubStr(String[] s,String[] t,int n,int m)
+    { 
+        int dp[][]=new int[2][m+1];
+        int res=0;
+      
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
             {
-                if(pattern[j].equals(txt[i]))
+                if(s[(i-1)].equals(t[j-1]))
                 {
-                    System.out.println("Matching");
-                    j++;
+                    dp[i%2][j]=dp[(i-1)%2][j-1]+1;
+                    if(dp[i%2][j]>res)
+                    {
+                        res=dp[i%2][j];
+                    }
+                }
+                else 
+                {
+                    dp[i%2][j]=0;
+                }
+            }
+        }
+        return res;
+    }
+    String printLCSubStr(String [] X, String [] Y, int m, int n)
+    {
+        int[][] LCSuff = new int[m + 1][n + 1];
+ 
+        int len = 0;
+ 
+        int row = 0, col = 0;
+        for (int i = 0; i <= m; i++) 
+        {
+            for (int j = 0; j <= n; j++) 
+            {
+                if (i == 0 || j == 0)
+                {
+                    LCSuff[i][j] = 0;
+                }
+                else if (X[i-1].equals(Y[j-1])) 
+                {
+                    LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                    if (len < LCSuff[i][j]) 
+                    {
+                        len = LCSuff[i][j];
+                        row = i;
+                        col = j;
+                    }
                 }
                 else
                 {
-                    i++;
+                    LCSuff[i][j] = 0;
                 }
             }
+        }
+        if (len == 0) 
+        {
+            return " ";
+        }
+        String resultStr = "";
+        while (LCSuff[row][col] != 0) 
+        {
+            resultStr = X[row - 1] + resultStr; 
+            --len;
+ 
+            row--;
+            col--;
+        }
+        return resultStr;
+    }
+    int prefix(String txt, String pat)
+    {
+        int len=0;
+        boolean flag=false;
+        String str = "";
+        int n=pat.length();
+        for(int i=0;i<=n;i++)
+        {
+            str=pat.substring(0,i);
+            if(txt.contains(str))
+            {
+                flag=true;
+                len=str.length();
+            }
+        }
+        if(flag==true)
+        {
+            return len;
+        }
+        else
+        {
+            return 0;
         }
     }
 }
