0e4b5fb09e7dafe0896f924a7d1144a8d355997a
Devika Manoj <dmenon138@gmail.com>
2021-10-21 21:51:04 +0530

    5TC passed

--- SpellingCorrections/src/U4AIE20144.java
+++ SpellingCorrections/src/U4AIE20144.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class U4AIE20144 
 {
     static String str;
+    static ArrayList<String> newstr ;
+    static int len;
+    static String[] string;
     public static void main(String[] args) 
     {
         Scanner in=new Scanner(System.in);
@@ -10,16 +13,12 @@ public class U4AIE20144
         for(int i=0;i<n;i++)
         {
             str=in.nextLine();
-            str=str.toLowerCase(); 
-            String[] string = str.split(" ");
+            //str=str.toLowerCase(); 
+            string = str.split(" ");
             String ts=string[1];
-            int len=Integer.parseInt(ts);
+            len=Integer.parseInt(ts);
 
-            ArrayList<String> newstr = new ArrayList<String>(len+2);
-            for(int j=2;j<len+2;j++)
-            {
-                newstr.add(string[j]);
-            }
+            ArrayListInsert();
           //  System.out.print(newstr+" ");
             if(string[0].startsWith("a"))
             {
@@ -29,7 +28,6 @@ public class U4AIE20144
                     SpellingCorrection.insert(newstr.get(j),j);
                 }
             }
-            SpellingCorrection.print();
             if(string[0].startsWith("d"))
             {
                 for(int j=0;j<len;j++)
@@ -45,14 +43,23 @@ public class U4AIE20144
                 }
             }
         }
-        SpellingCorrection.print();
+       // SpellingCorrection.print();
         in.close();
+    }
+    static void ArrayListInsert()
+    {
+        newstr = new ArrayList<String>(len+2);
+        for(int j=2;j<len+2;j++)
+        {
+            newstr.add(string[j]);
+        }
     }
 }
 class SpellingCorrection
 {
     static Hashtable<String,Integer> HT = new Hashtable<String,Integer>();
-
+    static int count=0;
+    static String newstr="";
     static void insert(String string,int j)
     {
         HT.put(string,j);
@@ -63,18 +70,157 @@ class SpellingCorrection
     }
     static void find(String string)
     {
-        //HT.containsKey(string);
-    }
-    static void print()
-    {
-        System.out.println("maps");
-        for (Map.Entry<String, Integer> entry : HT.entrySet()) 
+        if(HT.containsKey(string))
         {
-            String key = entry.getKey();
+            System.out.print("Y"+" ");
+        }
+        else
+        {
+            replacealphabet(string);
+            insertSpace(string);
+            swappingnei(string);
+            removeelem(string);
+            replaceElem(string);
+            System.out.print(count+" ");
+            count=0;
+        }
+    }
+    static void removeelem(String str)
+    {
+        String[] strSplit = str.split("");
+        ArrayList<String> strList = new ArrayList<String>(Arrays.asList(strSplit));
+        for(int i=0;i<strList.size();i++)
+        {
+            String temp=strList.get(i);
+            strList.remove(strList.get(i));
+            String newstr=listtoString(strList);
+            strList.add(i,temp);
+            if(HT.containsKey(newstr))
+            {
+               count++;
+            }
+        }
 
-            System.out.print(" " + key );
+    }
+
+    static void swappingnei(String str)
+    {
+        String[] strSplit = str.split("");
+        
+        ArrayList<String> strList = new ArrayList<String>(Arrays.asList(strSplit));
+
+        for(int i=0;i<strList.size();i++)
+        {
+            if(i+1<strList.size())
+            {
+                Collections.swap(strList, i, i+1);
+                newstr=listtoString(strList);
+                Collections.swap(strList, i+1, i);
+                if(HT.containsKey(newstr))
+                {
+                   count++;
+                } 
+            }
+        }
+    }
+    static String listtoString(ArrayList<String>strList)
+    {
+        String list = Arrays.toString(strList.toArray()).replace("[", "").replace("]", ""); 
+        list = list.replaceAll(", ", "");
+        return list;
+    }
+    static void insertSpace(String str)
+    {
+        String x,y;
+        for(int i=1;i<str.length();i++)
+        {
+            x=str.substring(0, i);
+            y=str.substring(i, str.length());
+            if(HT.containsKey(x)||HT.containsKey(y))
+            {
+                count++;
+            }
+        }
+    }
+    static void replacealphabet(String str)
+    {
+        char c;
+        String newstr,x,y;
+
+        for(int i=0;i<str.length();i++)
+        {
+            x=str.substring(0, i);
+            y=str.substring(i,str.length());
+
+            for(c = 'a'; c <= 'z'; ++c)
+            {
+                newstr=x+c+y;
+                if(HT.containsKey(newstr))
+                {
+                    count++;
+                }
+                else
+                {
+                    newstr=str;
+                }
+            }
+        }
+        x=str.substring(0, str.length()-1);
+        y=str.substring(str.length()-1, str.length());
+        for(c = 'a'; c <= 'z'; ++c)
+        {
+            newstr=x+c+y;
+            if(HT.containsKey(newstr))
+            {
+                count++;
+            }
+            else
+            {
+                newstr=str;
+            }
+        }
+        for(c = 'a'; c <= 'z'; ++c)
+        {
+            newstr=str+c;
+            if(HT.containsKey(newstr))
+            {
+                count++;
+            }
+            else
+            {
+                newstr=str;
+            }
+        }
+    }
+    static void replaceElem(String str)
+    {
+        char c;
+        String newstr;
+        
+        String[] strSplit = str.split("");
+        
+        ArrayList<String> strList = new ArrayList<String>(Arrays.asList(strSplit));
+        
+        String[] temp=new String[strList.size()];
+        strList.toArray(temp);
+ 
+      //  Collections.copy(strList, temp);
+        for(int i=0;i<strList.size();i++)
+        {
+           strList = new ArrayList<String>(Arrays.asList(temp));
+
+            for(c = 'a'; c <= 'z'; ++c)
+            {
+                String S=Character.toString(c);
+                strList.set(i, S);
+                newstr=listtoString(strList);
+                if(HT.containsKey(newstr))
+                {
+                   count++;
+                } 
+            }
 
         }
-        System.out.println();
+
     }
 }
