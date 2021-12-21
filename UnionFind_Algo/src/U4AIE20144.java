be3d07fe40cc48560d303d5f09819219635a05e0
Devika Manoj <dmenon138@gmail.com>
2021-12-21 19:19:37 +0530

    Added new assignment

---
+++ UnionFind_Algo/src/U4AIE20144.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class U4AIE20144
+{
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception 
+    {
+        String elem = in.nextLine();
+        String branch = in.nextLine();
+        System.out.println(elem);
+        System.out.println(branch);
+    }
+}
+class UnionFind
+{
+
+}
