7a39614ecd57f0648df14b3b70e30ca35f8d8879
Devika Manoj <dmenon138@gmail.com>
2021-12-22 19:36:39 +0530

    methods added

--- UnionFind_Algo/src/U4AIE20144.java
+++ UnionFind_Algo/src/U4AIE20144.java
@@ -5,12 +5,51 @@ public class U4AIE20144
     public static void main(String[] args) throws Exception 
     {
         String elem = in.nextLine();
-        String branch = in.nextLine();
-        System.out.println(elem);
-        System.out.println(branch);
+        String bran = in.nextLine();
+        ArrayList<Integer> Elements=new ArrayList<Integer>();
+        ArrayList<Integer> Branches=new ArrayList<Integer>();
+        Elements=StrToArray(elem);
+        Branches=StrToArray(bran);
+        pairs(Branches);
+
+    }
+    static void pairs(ArrayList<Integer> Branches)
+    {
+        for(int i=0;i<Branches.size();i++)
+        {
+            int P1=Branches.get(i);
+            i++;
+            int P2=Branches.get(i);
+            UnionFind.union(P1, P2);
+        }
+    }
+    static ArrayList<Integer> StrToArray(String str)
+    {
+        String temp[]=str.split(" ");
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        for(int i =0;i<temp.length;i++)
+        {
+            arr.add(Integer.valueOf(temp[i]));
+        }
+        return arr;
     }
 }
 class UnionFind
 {
-
+    static ArrayList<Integer> Tree=new ArrayList<Integer>();
+    static void union(int a, int b)
+    {
+        // find the root of the sets in which elements `x` and `y` belongs
+        int x = find(a);
+        int y = find(b);
+         
+        if(x>=y)
+        {
+            int temp =x;
+            x=y;
+            y=temp;
+        }
+        parent[x]=y;
+        
+    }
 }
